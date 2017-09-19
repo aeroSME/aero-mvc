86c807a096c4f5bf4fbffeb9b3cf0b1426d8f71e
aeroSME <kevin.arthur.meyer@gmail.com>
2017-09-18 20:41:49 -0500

    Cleaned up code on WPT Controller

--- src/main/java/com/example/aeromvc/controllers/WaypointController.java
+++ src/main/java/com/example/aeromvc/controllers/WaypointController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
 @Controller
@@ -16,13 +15,7 @@ public class WaypointController {
 
     static         ArrayList<String> waypoints = new ArrayList<>();
     @RequestMapping(value = "")
-//    @ResponseBody
-//    public String index(HttpServletRequest request) {
-//
-//        String ident = request.getParameter("ident");
-//
-//        return "Waypoint " + ident;
-//    }
+
     public String index(Model model) {
 
         model.addAttribute("title", "Waypoints");

a06d8f73287eba828441172ac0c2753e2f6a8cfb
aeroSME <kevin.arthur.meyer@gmail.com>
2018-03-05 14:50:05 -0600

    Reorganized Login/Security.

--- src/main/java/com/example/aeromvc/controllers/LoginController.java
+++ src/main/java/com/example/aeromvc/controllers/LoginController.java
@@ -1,4 +1,17 @@
 package com.example.aeromvc.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("login")
 public class LoginController {
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute("title", "Please Log In");
+        return "login/index";
+    }
 }
---
+++ src/main/java/com/example/aeromvc/security/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.example.aeromvc.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/main/index").setViewName("main");
+        registry.addViewController("/").setViewName("main");
+        //registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
